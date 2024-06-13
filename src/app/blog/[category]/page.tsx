21b06a9b86bd79edb49cef67035ca7d774f732b3
DayTeaJun <gkfnck@gmail.com>
2024-06-13 23:54:35 +0900

    Design: 게시물 목록 임시 추가 및 섬네일 추가

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import { getPostList } from '@/lib/post';
+import Image from 'next/image';
+import Link from 'next/link';
 
-function CategoryList() {
-  return <div>page</div>;
-}
+type Props = {
+  params: { category: string };
+};
 
-export default CategoryList;
+const Category = async ({ params }: Props) => {
+  const postList = await getPostList(params.category);
+
+  return (
+    <section className=" mx-auto w-[900px] h-[100vh] mt-8">
+      <ul className="flex flex-row justify-between w-full">
+        {postList.map((post) => (
+          <li
+            key={post.url}
+            className="w-[400px] h-[350px] border  border-neutral-700 rounded-xl cursor-pointer"
+          >
+            <Link
+              className=" w-full h-full  flex flex-col gap-3"
+              href={post.url}
+            >
+              <div className=" relative aspect-video bg-neutral-600 rounded-t-xl">
+                <Image
+                  alt={post.title}
+                  src={post.thumbnail}
+                  fill
+                  sizes="500px, 450px"
+                  style={{
+                    objectFit: 'cover',
+                    padding: '16px',
+                  }}
+                />
+              </div>
+              <div className="flex flex-col gap-4 justify-between items-center">
+                <h2>{post.title}</h2>
+                <p>{post.desc}</p>
+                <p>{post.dateString}</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Category;
