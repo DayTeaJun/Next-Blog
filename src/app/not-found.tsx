7d472015090519c1ab638b17ae4ec6768b58904d
DayTeaJun <gkfnck@gmail.com>
2024-07-10 23:59:18 +0900

    Design: 404 page 디자인

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,9 @@
 function NotFound() {
-	return <section>찾을 수 없는 페이지입니다.</section>;
+	return (
+		<section className='h-full p-8 font-bold text-center flex flex-col gap-4 items-center'>
+			<p>404</p>
+			<p>찾을 수 없는 페이지입니다.</p>
+		</section>
+	);
 }
 export default NotFound;
